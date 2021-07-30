8238f447bdcf5138e470743500a21e48ccb9740d
becoswap-dev <cool@becoswap.com>
2021-07-30 17:23:01 +0700

    fix mint artwork nft

--- contracts/artwork/ArtworkNFT.sol
+++ contracts/artwork/ArtworkNFT.sol
@@ -18,43 +18,63 @@ contract ArtworkNFT is ERC721URIStorage, Ownable {
     address public mintFeeAddr;
     uint256 public mintFeeAmount;
     mapping(uint256 => Profile) public profiles;
-    
+
     struct Profile {
         address creator;
         uint256 royalties;
     }
-    
+
     event UpdatedProfile(uint256 tokenId, address creator, uint256 royalties);
-    
-    constructor(string memory _name, string memory _symbol, address _mintFeeAddr, uint256 _mintFeeAmount) ERC721(_name, _symbol) Ownable() {
-       mintFeeAddr = _mintFeeAddr;
-       mintFeeAmount = _mintFeeAmount;
+
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        address _mintFeeAddr,
+        uint256 _mintFeeAmount
+    ) ERC721(_name, _symbol) Ownable() {
+        mintFeeAddr = _mintFeeAddr;
+        mintFeeAmount = _mintFeeAmount;
     }
-    
-    function setProfile(uint256 tokenId, address _creator, uint256 _royalties) public {
+
+    function setProfile(
+        uint256 tokenId,
+        address _creator,
+        uint256 _royalties
+    ) public {
         require(profiles[tokenId].creator == _msgSender(), "only creator");
         require(_royalties <= MAX_ROYALTIES, "max royalties");
         _setProfile(tokenId, _creator, _royalties);
     }
-    
-    function _setProfile(uint256 tokenId, address _creator, uint256 _royalties) private {
+
+    function _setProfile(
+        uint256 tokenId,
+        address _creator,
+        uint256 _royalties
+    ) private {
         profiles[tokenId].creator = _creator;
         profiles[tokenId].royalties = _royalties;
         emit UpdatedProfile(tokenId, _creator, _royalties);
     }
-  
-    
-    function mint(address _to, string memory _tokenURI, uint256 _royalties) public payable returns (uint256 tokenId) {
-        require(msg.value >= mintFeeAmount, 'msg value too low');
+
+    function mint(
+        address _to,
+        string memory _tokenURI,
+        uint256 _royalties
+    ) public payable returns (uint256 tokenId) {
+        require(msg.value >= mintFeeAmount, "msg value too low");
         TransferHelper.safeTransferETH(mintFeeAddr, mintFeeAmount);
         tokenId = nextTokenId;
         _mint(_to, tokenId);
         nextTokenId++;
-        _setProfile(tokenId, _msgSender(), _royalties);
+        _setProfile(tokenId, _to, _royalties);
         _setTokenURI(tokenId, _tokenURI);
-        if (msg.value > mintFeeAmount) TransferHelper.safeTransferETH(msg.sender, msg.value - mintFeeAmount);
+        if (msg.value > mintFeeAmount)
+            TransferHelper.safeTransferETH(
+                msg.sender,
+                msg.value - mintFeeAmount
+            );
     }
-    
+
     /**
      * @dev Burns `tokenId`. See {ERC721-_burn}.
      *
@@ -64,15 +84,18 @@ contract ArtworkNFT is ERC721URIStorage, Ownable {
      */
     function burn(uint256 tokenId) public virtual {
         //solhint-disable-next-line max-line-length
-        require(_isApprovedOrOwner(_msgSender(), tokenId), "ERC721Burnable: caller is not owner nor approved");
+        require(
+            _isApprovedOrOwner(_msgSender(), tokenId),
+            "ERC721Burnable: caller is not owner nor approved"
+        );
         _burn(tokenId);
         delete profiles[tokenId];
     }
-    
+
     function setMintFeeAddr(address _mintFeeAddr) public onlyOwner {
         mintFeeAddr = _mintFeeAddr;
     }
-    
+
     function setMintFeeAmount(uint256 _mintFeeAmount) public onlyOwner {
         mintFeeAmount = _mintFeeAmount;
     }
