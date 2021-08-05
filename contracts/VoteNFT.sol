f430e1c939f3cd6224bb1a87b767156ead2cc378
becoswap-dev <cool@becoswap.com>
2021-08-05 09:26:24 +0700

    test vote nft

--- contracts/VoteNFT.sol
+++ contracts/VoteNFT.sol
@@ -11,13 +11,13 @@ contract VoteNFT is Ownable {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
 
-    IERC20 public voteToken;
+    IERC20 public krc20;
     address public feeAddress;
 
     event Voted(address voter, address nft, uint256 tokenId, uint256 votes);
 
-    constructor(address _voteToken, address _feeAddress) Ownable() {
-        voteToken = IERC20(_voteToken);
+    constructor(address _krc20, address _feeAddress) Ownable() {
+        krc20 = IERC20(_krc20);
         feeAddress = _feeAddress;
     }
 
@@ -26,10 +26,10 @@ contract VoteNFT is Ownable {
         uint256 _tokenId,
         uint256 _votes
     ) public {
-        uint256 half = _votes.div(2);
         IERC721 nft = IERC721(_nft);
-        voteToken.safeTransferFrom(_msgSender(), nft.ownerOf(_tokenId), half);
-        voteToken.safeTransferFrom(_msgSender(), feeAddress, half);
+        uint256 half = _votes.div(2);
+        krc20.safeTransferFrom(_msgSender(), nft.ownerOf(_tokenId), half);
+        krc20.safeTransferFrom(_msgSender(), feeAddress, half);
         emit Voted(_msgSender(), _nft, _tokenId, _votes);
     }
 
---
+++ test/VoteNFT.js
@@ -0,0 +1,32 @@
+const ArtworkNFT = artifacts.require("./artwork/ArtworkNFT.sol");
+const VoteNFT = artifacts.require("./VoteNFT.sol");
+const TestErc20 = artifacts.require("./test/TestErc20.sol");
+const { expectRevert } = require('@openzeppelin/test-helpers');
+
+const dead = "0x000000000000000000000000000000000000dead";
+
+
+contract("VoteNFT", ([owner, voter]) => {
+    beforeEach(async () => {
+        this.nft = await ArtworkNFT.new("Demo", "DEMO", dead, 5);
+        this.erc20 = await TestErc20.new()
+        this.vote = await VoteNFT.new(this.erc20.address, dead);
+
+        await this.nft.mint(owner, "", 5, {value: 5});
+
+        // mint token for voter
+        await this.erc20.mint(100, {from: voter})
+        await this.erc20.approve(this.vote.address, 80, {from: voter});
+    })
+
+    it("vote", async () => {
+        await expectRevert(this.vote.vote(this.nft.address, 2, 100, { from: voter}), "owner query for nonexistent token")
+        await expectRevert(this.vote.vote(this.nft.address, 1, 100, { from: voter}), "transfer amount exceeds allowance")
+        await this.vote.vote(this.nft.address, 1, 80, { from: voter});
+        await expectRevert(this.vote.vote(this.nft.address, 1, 50, { from: voter}), "transfer amount exceeds balance")
+    })
+
+    it("setFeeAddress", async() => {
+        await expectRevert(this.vote.setFeeAddress(voter, {from: voter}), "caller is not the owner")
+    })
+})
