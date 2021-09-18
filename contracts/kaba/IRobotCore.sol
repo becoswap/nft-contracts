f3fa36d1ce5956a0196ce50ad221f0f724a1dc1e
becoswap-dev <cool@becoswap.com>
2021-09-18 14:36:06 +0700

    add kaba

---
+++ contracts/kaba/IRobotCore.sol
@@ -0,0 +1,8 @@
+pragma solidity =0.8.0;
+pragma experimental ABIEncoderV2;
+
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+
+interface IRobotCore is IERC721 {
+    function isPregnant(uint256 _robotId) public view returns (bool);
+}
