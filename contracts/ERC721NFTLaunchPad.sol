dd7f65ca9a9f79b753a86de6b381fa7c220ea82a
beco <>
2022-01-14 17:58:19 +0700

    update

--- contracts/ERC721NFTLaunchPad.sol
+++ contracts/ERC721NFTLaunchPad.sol
@@ -5,6 +5,7 @@ pragma abicoder v2;
 
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 interface ICredit {
     function getCredit(address user) external view returns (uint256);
@@ -14,7 +15,7 @@ interface ILaunchpadMinter {
     function mint(address user, uint256 level);
 }
 
-contract ERC721NFTLaunchPad is ReentrancyGuard {
+contract ERC721NFTLaunchPad is ReentrancyGuard, Ownable {
     struct Launch {
         uint256 price;
         uint256 maxSell;
@@ -23,8 +24,10 @@ contract ERC721NFTLaunchPad is ReentrancyGuard {
         uint256 creditPrice;
     }
 
-    address public creditAdd;
+    address public creditAddr;
     address public lauchpadMinter;
+    address public treasuryAddress;
+    address public dealToken;
 
     // user address => launchIndex => ask
     mapping(address => mapping(uint256 => uint256)) boughtCount;
@@ -32,12 +35,24 @@ contract ERC721NFTLaunchPad is ReentrancyGuard {
 
     event Buy(address indexed user, uint256 _launchIndex);
 
+    constructor(
+        address _creditAddr,
+        address _lauchpadMinter,
+        address _treasuryAddress,
+        address _dealToken
+    ) {
+        creditAddr = _creditAddr;
+        lauchpadMinter = _lauchpadMinter;
+        treasuryAddress = _treasuryAddress;
+        dealToken = _dealToken;
+    }
+
     function addLaunch(
         uint256 _price,
         uint256 _maxSell,
         uint256 _level,
         uint256 _creditPrice
-    ) external returns (uint256) {
+    ) external onlyOwner nonReentrant returns (uint256) {
         launches.push(
             Launch({
                 price: _price,
@@ -53,28 +68,24 @@ contract ERC721NFTLaunchPad is ReentrancyGuard {
      * @notice buy NFT
      * @param _launchIndex: launchpad index
      */
-    function buy(uint256 _launchIndex) external {
-        require(launches.length > _launchIndex, "launchpad not found");
-        require(
-            launches[_launchIndex].totalSold < launches[_launchIndex].maxSell,
-            "sold out"
-        );
-        _checkCredit(_launchIndex);
-        ILaunchpadMinter(lauchpadMinter).mint(
-            msg.sender,
-            launches[_launchIndex].level
-        );
+    function buy(uint256 _launchIndex) external nonReentrant {
+        _checkLimit(_launchIndex);
+        Launch storage launch = launches[_launchIndex];
+        ILaunchpadMinter(lauchpadMinter).mint(msg.sender, launch.level);
         boughtCount[msg.sender][_launchIndex]++;
-        launches[_launchIndex].totalSold++;
+        launch.totalSold++;
+        IERC20(dealToken).safeTransferFrom(msg.sender, treasuryAddress, launch.price);
         Buy(msg.sender, _launchIndex);
     }
 
-    function _checkCredit(uint256 _launchIndex) private {
+    function _checkLimit(uint256 _launchIndex) private {
+        Launch memory launch = launches[_launchIndex];
         uint256 creditAmount = ICredit(creditAdd).getCredit(msg.sender);
-        uint256 maxCanBuy = creditAmount / launches[_launchIndex].creditPrice;
+        uint256 maxCanBuy = creditAmount / launch.creditPrice;
         require(
             maxCanBuy >= boughtCount[msg.sender][_launchIndex],
             "max can buy"
         );
+        require(launch.totalSold < launch.maxSell, "sold out");
     }
 }
