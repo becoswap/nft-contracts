279461f3b47e3fb64e11a690c265db1b5e2c2339
beco <>
2022-01-17 16:02:37 +0700

    check max total fee

--- contracts/Erc721NFTFeeDistributor.sol
+++ contracts/Erc721NFTFeeDistributor.sol
@@ -17,7 +17,8 @@ contract Erc721NFTFeeDistributor is Ownable {
     address public protocolFeeRecipient;
     uint256 public protocolFeePercent = 250;
 
-    uint256 public MAX_FEE = 500; // 10%
+    uint256 public MAX_FEE = 500; // 5%
+    uint256 public MAX_TOTAL_FEE = 1000; // 10%
 
     constructor(address _feeProvider, address _recipient, uint256 _feePercent) {
         feeProvider = _feeProvider;
@@ -61,7 +62,13 @@ contract Erc721NFTFeeDistributor is Ownable {
         address[] memory _addrs;
         uint256[] memory _rates;
         (_addrs, _rates) = IFeeProvider(feeProvider).getFees(_nft, _tokenId);
+        uint256 sumRates = 0;
         for (uint256 i = 0; i < _addrs.length; i++) {
+            sumRates = sumRates.add(_rates[i]);
+            if (sumRates > MAX_TOTAL_FEE) {
+                return sumFees;
+            }
+
             fee = _price.mul(_rates[i]).div(10000);
             IERC20(_quoteToken).safeTransfer(_addrs[i], fee);
             sumFees = sumFees.add(fee);
