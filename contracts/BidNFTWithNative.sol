e940d4c23ac197fa52fd6c6cdceb1dbcfeb8466e
beco <>
2022-03-01 12:37:47 +0700

    update

--- contracts/BidNFTWithNative.sol
+++ contracts/BidNFTWithNative.sol
@@ -12,8 +12,10 @@ import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/utils/structs/EnumerableMap.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "./libraries/TransferHelper.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract BidNFTWithNative is ERC721Holder, Ownable, Pausable {
+
+contract BidNFTWithNative is ERC721Holder, Ownable, Pausable, ReentrancyGuard {
     using SafeMath for uint256;
     using Address for address;
     using EnumerableSet for EnumerableSet.AddressSet;
@@ -25,13 +27,24 @@ contract BidNFTWithNative is ERC721Holder, Ownable, Pausable {
 
     IERC721 public nft;
     address public feeAddr;
-    uint256 public feePercent;
+    uint256 public feePercent = 250;
+    uint256 public royaltyFeePercent = 250;
+    address public royaltyFeeRecipient;
+    uint256 private _MAX_FEE = 1000;
+
     mapping(uint256 => uint256) public prices;
     mapping(uint256 => address) public sellers;
     mapping(uint256 => mapping(address => uint256)) userBidPrice;
     mapping(uint256 => EnumerableSet.AddressSet) private tokenBids;
 
     event Trade(
+        address indexed seller,
+        address indexed buyer,
+        uint256 indexed tokenId,
+        uint256 price,
+        uint256 fee
+    );
+    event BidAccept(
         address indexed seller,
         address indexed buyer,
         uint256 indexed tokenId,
@@ -60,17 +73,21 @@ contract BidNFTWithNative is ERC721Holder, Ownable, Pausable {
     constructor(
         address _nftAddress,
         address _feeAddr,
-        uint256 _feePercent
+        uint256 _feePercent,
+        address _royaltyFeeRecipient,
+        uint256 _royaltyFeePercent
     ) public {
         require(_nftAddress != address(0) && _nftAddress != address(this));
         nft = IERC721(_nftAddress);
         feeAddr = _feeAddr;
         feePercent = _feePercent;
+        royaltyFeePercent = _royaltyFeePercent;
+        royaltyFeeRecipient = _royaltyFeeRecipient;
         emit UpdatedFeeAddress(address(0), feeAddr);
         emit SetFeePercent(_msgSender(), 0, feePercent);
     }
 
-    function buyToken(uint256 _tokenId) public payable whenNotPaused {
+    function buyToken(uint256 _tokenId) public payable whenNotPaused nonReentrant{
         buyTokenTo(_tokenId, _msgSender());
     }
 
@@ -78,18 +95,16 @@ contract BidNFTWithNative is ERC721Holder, Ownable, Pausable {
         public
         payable
         whenNotPaused
+        nonReentrant
     {
         uint256 price = prices[_tokenId];
         require(price > 0, "Token not in sell book");
         require(msg.value >= price, "invalid value");
 
         nft.safeTransferFrom(address(this), _to, _tokenId);
-        uint256 feeAmount = price.mul(feePercent).div(100);
-        if (feeAmount != 0) {
-            TransferHelper.safeTransferETH(feeAddr, feeAmount);
-        }
-
-        TransferHelper.safeTransferETH(sellers[_tokenId], price.sub(feeAmount));
+        uint256 sumFees = _distributeFees(price);
+        uint256 netPrice = price.sub(sumFees);
+        TransferHelper.safeTransferETH(sellers[_tokenId], netPrice);
         address seller = sellers[_tokenId];
 
         delete prices[_tokenId];
@@ -99,20 +114,21 @@ contract BidNFTWithNative is ERC721Holder, Ownable, Pausable {
             TransferHelper.safeTransferETH(_msgSender(), msg.value.sub(price));
         }
 
-        emit Trade(seller, _msgSender(), _tokenId, price, feeAmount);
+        emit Trade(seller, _msgSender(), _tokenId, price, sumFees);
     }
 
     function setCurrentPrice(uint256 _tokenId, uint256 _price)
         public
         whenNotPaused
         onlySeller(_tokenId)
+        nonReentrant
     {
         require(_price != 0, "Price must be granter than zero");
         prices[_tokenId] = _price;
         emit Ask(_msgSender(), _tokenId, _price);
     }
     
-    function batchSetCurrentPrice(uint256[] calldata _tokenIds, uint256[] calldata _prices) public {
+    function batchSetCurrentPrice(uint256[] calldata _tokenIds, uint256[] calldata _prices) public nonReentrant{
         require(_tokenIds.length == _prices.length);
         for (uint i = 0; i < _tokenIds.length; i++) {
             setCurrentPrice(_tokenIds[i], _prices[i]);
@@ -122,11 +138,12 @@ contract BidNFTWithNative is ERC721Holder, Ownable, Pausable {
     function readyToSellToken(uint256 _tokenId, uint256 _price)
         public
         whenNotPaused
+        nonReentrant
     {
         readyToSellTokenTo(_tokenId, _price, address(_msgSender()));
     }
     
-    function batchReadyToSellToken(uint256[] calldata _tokenIds, uint256[] calldata _prices) public {
+    function batchReadyToSellToken(uint256[] calldata _tokenIds, uint256[] calldata _prices) public nonReentrant{
         require(_tokenIds.length == _prices.length);
         for (uint i = 0; i < _tokenIds.length; i++) {
             readyToSellToken(_tokenIds[i], _prices[i]);
@@ -137,7 +154,7 @@ contract BidNFTWithNative is ERC721Holder, Ownable, Pausable {
         uint256 _tokenId,
         uint256 _price,
         address _to
-    ) public whenNotPaused {
+    ) public whenNotPaused nonReentrant{
         require(!_isContract(_msgSender()), "Contract call not allow");
         require(
             _msgSender() == nft.ownerOf(_tokenId),
@@ -156,6 +173,7 @@ contract BidNFTWithNative is ERC721Holder, Ownable, Pausable {
         public
         whenNotPaused
         onlySeller(_tokenId)
+        nonReentrant
     {
         nft.safeTransferFrom(address(this), _msgSender(), _tokenId);
         delete prices[_tokenId];
@@ -163,13 +181,13 @@ contract BidNFTWithNative is ERC721Holder, Ownable, Pausable {
         emit CancelSellToken(_msgSender(), _tokenId);
     }
     
-    function batchCancelSellToken(uint256[] calldata _tokenIds) public {
+    function batchCancelSellToken(uint256[] calldata _tokenIds) public nonReentrant{
         for (uint i = 0; i < _tokenIds.length; i ++) {
             cancelSellToken(_tokenIds[i]);
         }
     }
 
-    function bidToken(uint256 _tokenId) public payable whenNotPaused {
+    function bidToken(uint256 _tokenId) public payable whenNotPaused nonReentrant{
         require(userBidPrice[_tokenId][_msgSender()] == 0, "Bidder found");
         require(msg.value != 0, "Price must be granter than zero");
         userBidPrice[_tokenId][_msgSender()] = msg.value;
@@ -177,25 +195,46 @@ contract BidNFTWithNative is ERC721Holder, Ownable, Pausable {
         emit Bid(_msgSender(), _tokenId, msg.value);
     }
 
+    function _distributeFees(uint256 price) private returns (uint256) {
+        uint256 sumFees = 0;
+
+        if (feeAddr != address(0)) {
+            uint256 feeAmount = price.mul(feePercent).div(10000);
+            if (feeAmount != 0) {
+                sumFees = feeAmount;
+                TransferHelper.safeTransferETH(feeAddr, feeAmount);
+            }
+            
+        }
+
+        if (royaltyFeeRecipient != address(0)) {
+            uint256 royaltyFee = price.mul(royaltyFeePercent).div(10000);
+            if (royaltyFee != 0) {
+                sumFees = sumFees.add(royaltyFee);
+                TransferHelper.safeTransferETH(royaltyFeeRecipient, royaltyFee);
+            }
+        }
+
+        return sumFees;
+    }
+
     function sellTokenTo(
         uint256 _tokenId,
         address _to,
         uint256 _price
-    ) public whenNotPaused onlySeller(_tokenId) {
+    ) public whenNotPaused onlySeller(_tokenId) nonReentrant{
         uint256 price = getUserBidPriceAndRemove(_tokenId, _to);
         require(price == _price, "invalid price");
         nft.safeTransferFrom(address(this), _to, _tokenId);
 
-        uint256 feeAmount = price.mul(feePercent).div(100);
-        if (feeAmount != 0) {
-            TransferHelper.safeTransferETH(feeAddr, feeAmount);
-        }
-        TransferHelper.safeTransferETH(sellers[_tokenId], price.sub(feeAmount));
+        uint256 sumFees = _distributeFees(price);
+        uint256 netPrice = price.sub(sumFees);
+        TransferHelper.safeTransferETH(sellers[_tokenId], netPrice);
         address seller = sellers[_tokenId];
         delete prices[_tokenId];
         delete sellers[_tokenId];
 
-        emit Trade(seller, _to, _tokenId, price, feeAmount);
+        emit BidAccept(seller, _to, _tokenId, price, sumFees);
     }
 
     function getUserBidPriceAndRemove(uint256 _tokenId, address bidder)
@@ -209,12 +248,12 @@ contract BidNFTWithNative is ERC721Holder, Ownable, Pausable {
         return price;
     }
 
-    function cancelBidToken(uint256 _tokenId) public whenNotPaused {
+    function cancelBidToken(uint256 _tokenId) public whenNotPaused nonReentrant{
         require(userBidPrice[_tokenId][_msgSender()] > 0, "Bidder not found");
         uint256 price = userBidPrice[_tokenId][_msgSender()];
-        TransferHelper.safeTransferETH(_msgSender(), price);
         userBidPrice[_tokenId][_msgSender()] = 0;
         tokenBids[_tokenId].remove(_msgSender());
+        TransferHelper.safeTransferETH(_msgSender(), price);
         emit CancelBidToken(_msgSender(), _tokenId);
     }
 
@@ -236,9 +275,18 @@ contract BidNFTWithNative is ERC721Holder, Ownable, Pausable {
     }
 
     function setFeePercent(uint256 _feePercent) public onlyOwner {
-        require(feePercent != _feePercent, "Not need update");
+        require(_feePercent <= _MAX_FEE, "MAX FEE");
         emit SetFeePercent(_msgSender(), feePercent, _feePercent);
         feePercent = _feePercent;
+    }
+
+    function setRoyaltyAddress(address _feeAddr) public onlyOwner {
+        royaltyFeeRecipient = _feeAddr;
+    }
+
+    function setRoyaltyFeePercent(uint256 _feePercent) public onlyOwner {
+        require(_feePercent <= _MAX_FEE, "MAX FEE");
+        royaltyFeePercent = _feePercent;
     }
 
     function pause() public onlyOwner whenNotPaused {
